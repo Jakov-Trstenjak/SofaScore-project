87fa680caf194a9299f223cd551775eff17d30fd
Jakov Trstenjak <jakov.trstenjak@talentlyft.com>
2021-05-31 09:16:44 +0200

    css update

--- sofa-score/src/components/Navbar.js
+++ sofa-score/src/components/Navbar.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import '../CSS/navbar.css';
+//import '../CSS/navbar.css';
 
 
 export default class Navbar extends Component {
     render() {
         return (
             <header>
-                <div>
-                    <a href="#/">
-                        <h1>SofaScoreAcademy</h1>
-                    </a>
+                <div class="topnav" id="myTopnav">
+                    <a href="" class="active">Home</a>
+                    <a href="">News</a>
+                    <a href="">Contact</a>
+                    <a href="">About</a>
+
                 </div>
+
             </header>
         )
     }

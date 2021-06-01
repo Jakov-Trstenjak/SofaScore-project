935e9e84c4f7c7f4478f6a6df3fdc1140c19b5a4
Jakov Trstenjak <jakov.trstenjak@talentlyft.com>
2021-06-01 11:57:07 +0200

    CSS update

--- sofa-score/src/components/Event.js
+++ sofa-score/src/components/Event.js
@@ -13,12 +13,58 @@ export default class Event extends Component {
     }
 
     render() {
+        const homeTeamLogoUrl = 'https://master.dev.sofascore.com/api/v1/team/'+ this.props.event.homeTeam.id+'/image';
+        const awayTeamLogoUrl = 'https://master.dev.sofascore.com/api/v1/team/'+ this.props.event.awayTeam.id+'/image';
+
+        const awayScore = this.props.event.awayScore;
+        const homeScore = this.props.event.homeScore;
+
         return (
-            <div className="event-container">
-                
-                <p>{this.props.event.homeTeam.name}</p>
-                <div className="versus"><p>-</p></div> 
-                <p>{this.props.event.awayTeam.name}</p>
+            <div className="event-container" title="Click for more information">
+                <table>
+                    <tr>
+                        <td className="event-time"> 
+                            <p>18:00</p>
+                        </td>
+                        <td className="team-name text-align-right">
+                            <p>{this.props.event.homeTeam.name}</p>
+                        </td>
+                        <td>
+                            <img className='team-logo ml8' src={homeTeamLogoUrl}></img>
+                        </td>
+                        <td>
+                            {
+                                (homeScore) ? (
+                                    <p className='scoreboard ml5'>{homeScore.display}</p>
+
+                                ) : (
+                                    <p className='scoreboard ml5'>{}</p>
+
+                                )
+                            }
+                        </td>
+                        <td className="versus">
+                            <p>-</p>
+                        </td>
+                        <td>
+                        {
+                                (awayScore) ? (
+                                    <p className='scoreboard mr5'>{awayScore.display}</p>
+
+                                ) : (
+                                    <p className='scoreboard mr5'>{}</p>
+
+                                )
+                            }
+                        </td>
+                        <td>
+                        <img className='team-logo mr8' src={awayTeamLogoUrl}></img>
+                        </td>
+                        <td className="team-name text-align-left">
+                            <p>{this.props.event.awayTeam.name}</p>
+                        </td>                   
+                    </tr>
+                </table>
             </div>
         )
     }
